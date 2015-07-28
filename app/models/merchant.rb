0dfeb1d218cc446446c79cd31b7c6bb4b7c713c3
Josh Cass <joshuacass@gmail.com>
2015-07-28 17:55:48 -0600

    wip

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,7 @@
 class Merchant < ActiveRecord::Base
+  include Finders
+
   has_many :items
   has_many :invoices
+  has_many :transactions, through: :invoices
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,6 @@
 class Transaction < ActiveRecord::Base
+  include Finders
+
   belongs_to :invoice
+  has_one :merchant, through: :invoice
 end
